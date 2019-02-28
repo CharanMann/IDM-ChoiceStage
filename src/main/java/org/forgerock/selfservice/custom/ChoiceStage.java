7b613f39c147f97afc801b7959f25628dcf76594
CharanMann <charan.mann@forgerock.com>
2019-02-28 14:35:34 -0500

    Initial commit

---
+++ src/main/java/org/forgerock/selfservice/custom/ChoiceStage.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright © 2018 ForgeRock, AS.
+ *
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Portions Copyrighted 2019 Charan Mann
+ *
+ * IDM-ChoiceStage: Created by Charan Mann on 2019-02-28 , 13:19.
+ */
+
+package org.forgerock.selfservice.custom;
+
+import org.forgerock.json.JsonValue;
+import org.forgerock.json.resource.BadRequestException;
+import org.forgerock.json.resource.ResourceException;
+import org.forgerock.selfservice.core.ProcessContext;
+import org.forgerock.selfservice.core.ProgressStage;
+import org.forgerock.selfservice.core.StageResponse;
+import org.forgerock.selfservice.core.util.RequirementsBuilder;
+
+import java.util.List;
+
+import static org.forgerock.json.JsonValue.json;
+import static org.forgerock.selfservice.core.util.RequirementsBuilder.oneOf;
+
+/**
+ * Choice stage to select require choice.
+ *
+ * @since 0.7.0
+ */
+public final class ChoiceStage implements ProgressStage<ChoiceStageConfig> {
+
+    @Override
+    public JsonValue gatherInitialRequirements(
+            ProcessContext context, ChoiceStageConfig config) throws ResourceException {
+
+        JsonValue[] choicesJson = generateChoiceJSONArray(config.getChoices());
+
+        return RequirementsBuilder
+                .newInstance("Choices")
+                .addRequireProperty("Choices", "Choices")
+                .addDefinition("Choices", oneOf(choicesJson))
+                .build();
+    }
+
+    private JsonValue[] generateChoiceJSONArray(List<String> choices) {
+        JsonValue[] choicesJson = new JsonValue[choices.size()];
+        int i = 0;
+
+        for (String choice : choices) {
+            choicesJson[i] = json(choice);
+            i++;
+        }
+
+        return choicesJson;
+    }
+
+    @Override
+    public StageResponse advance(
+            ProcessContext context, ChoiceStageConfig config) throws ResourceException {
+
+        JsonValue selectedChoice = context.getInput().get("selectedChoice");
+
+        if (selectedChoice.isNull()) {
+            throw new BadRequestException("No option selected");
+        }
+
+        context.putState(ChoiceStageConfig.SELECTED_CHOICE_FIELD, selectedChoice.asString());
+
+        return StageResponse.newBuilder().build();
+    }
+
+
+}
---
+++ src/main/java/org/forgerock/selfservice/custom/ChoiceStageConfig.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright © 2018 ForgeRock, AS.
+ *
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Portions Copyrighted 2019 Charan Mann
+ *
+ * IDM-ChoiceStage: Created by Charan Mann on 2019-02-28 , 13:19.
+ */
+
+package org.forgerock.selfservice.custom;
+
+import org.forgerock.selfservice.core.config.StageConfig;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Represents the config for a Choice stage
+ *
+ * @since 0.7.0
+ */
+public final class ChoiceStageConfig implements StageConfig {
+
+    /**
+     * State field for selected choice
+     */
+    public static final String SELECTED_CHOICE_FIELD = "selectedChoice";
+
+    private static final String NAME = "Choice";
+
+    private List<String> choices;
+
+    /**
+     * Gets the choices.
+     *
+     * @return choices
+     */
+    public List<String> getChoices() {
+        return choices;
+    }
+
+    /**
+     * Sets the choices
+     *
+     * @param choices
+     * @return this config
+     */
+    public ChoiceStageConfig setChoices(List<String> choices) {
+        this.choices = choices;
+        return this;
+    }
+
+    @Override
+    public String getProgressStageClassName() {
+        return ChoiceStage.class.getName();
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getProgressStageClassName(), choices);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof ChoiceStageConfig)) {
+            return false;
+        }
+
+        ChoiceStageConfig that = (ChoiceStageConfig) o;
+        return Objects.equals(getName(), that.getName())
+                && Objects.equals(getProgressStageClassName(), that.getProgressStageClassName())
+                && Objects.equals(choices, that.choices);
+    }
+}
